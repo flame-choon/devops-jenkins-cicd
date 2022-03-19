4783303b3022520af86c53e8b9457226952fd932
황광진 <kwangjin@hwang-gwangjins-MacBook-Pro.local>
2022-03-19 17:39:34 +0900

    Update Java

---
+++ Java/choon-java/src/main/java/choon/devops/jenkins/cicd/service/Greeting.java
@@ -0,0 +1,15 @@
+package choon.devops.jenkins.cicd.service;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content){
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() { return id; }
+    public String getContent() { return content; }
+}
