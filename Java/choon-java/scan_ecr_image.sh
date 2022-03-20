da4620e365d82ec9b000f8df1256f4955c56ac38
황광진 <kwangjin@hwang-gwangjins-MacBook-Pro.local>
2022-03-20 20:18:44 +0900

    Update

--- Java/choon-java/scan_ecr_image.sh
+++ Java/choon-java/scan_ecr_image.sh
@@ -10,41 +10,41 @@ LATEST_IMAGE_ID=$(cat list-images.json | jq -c '.imageIds[] | select(.imageTag =
 
 echo LATEST_IMAGE_ID : $LATEST_IMAGE_ID
 
-## wait for scan result
-#echo --- RUN aws ecr wait ---
-#
-#aws ecr wait image-scan-complete --repository-name $REPOSITORY_NAME --image-id imageDigest=$LATEST_IMAGE_ID
-#
-#echo --- COMPLETE aws ecr wait ---
-#
-## get scan result
-#echo --- RUN aws ecr describe-image-scan-findings ---
-#
-#aws ecr describe-image-scan-findings --repository-name $REPOSITORY_NAME --image-id imageDigest=$LATEST_IMAGE_ID > describe-image.json
-#
-#
-## find Severity Counts
-#echo --- find SeverityCounts ---
-#
-#SEVERITY_COUNTS_OBJECT=$(cat describe-image.json | jq '.imageScanFinding.findingSeverityCounts')
-#MEDIUM_VALUE=$(echo $SEVERITY_COUNTS_OBJECT | jq '.MEDIUM // 0')
-#HIGH_VALUE=$(echo $SEVERITY_COUNTS_OBJECT | jq '.HIGH // 0')
-#
-#echo MEDIUM_VALUE : $MEDIUM_VALUE
-#echo HIGH_VALUE : $HIGH_VALUE
-#
-## result Severity
-#echo --- result Severity ---
-#
-#SEVERITY_COUNTS=$(($MEDIUM_VALUE + $HIGH_VALUE))
-#
-#echo SEVERITY_COUNT : $SEVERITY_COUNTS
-#
-#if [ $SEVERITY_COUNTS -eq 0 ]
-#then
-#    echo "This stage COMPLETE"
-#else
-#    echo "Check severity"
-#    exit 1
-#fi
+# wait for scan result
+echo --- RUN aws ecr wait ---
+
+aws ecr wait image-scan-complete --repository-name $REPOSITORY_NAME --image-id imageDigest=$LATEST_IMAGE_ID
+
+echo --- COMPLETE aws ecr wait ---
+
+# get scan result
+echo --- RUN aws ecr describe-image-scan-findings ---
+
+aws ecr describe-image-scan-findings --repository-name $REPOSITORY_NAME --image-id imageDigest=$LATEST_IMAGE_ID > describe-image.json
+
+
+# find Severity Counts
+echo --- find SeverityCounts ---
+
+SEVERITY_COUNTS_OBJECT=$(cat describe-image.json | jq '.imageScanFinding.findingSeverityCounts')
+MEDIUM_VALUE=$(echo $SEVERITY_COUNTS_OBJECT | jq '.MEDIUM // 0')
+HIGH_VALUE=$(echo $SEVERITY_COUNTS_OBJECT | jq '.HIGH // 0')
+
+echo MEDIUM_VALUE : $MEDIUM_VALUE
+echo HIGH_VALUE : $HIGH_VALUE
+
+# result Severity
+echo --- result Severity ---
+
+SEVERITY_COUNTS=$(($MEDIUM_VALUE + $HIGH_VALUE))
+
+echo SEVERITY_COUNT : $SEVERITY_COUNTS
+
+if [ $SEVERITY_COUNTS -eq 0 ]
+then
+    echo "This stage COMPLETE"
+else
+    echo "Check severity"
+    exit 1
+fi
 
