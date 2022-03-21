d8135efe950e635cf2d8da5c6673b7efd803ec68
황광진 <kwangjin@hwang-gwangjins-MacBook-Pro.local>
2022-03-21 11:16:03 +0900

    Update

--- Java/choon-java/jenkins/scan_ecr_image.sh
+++ Java/choon-java/jenkins/scan_ecr_image.sh
@@ -1,47 +1,49 @@
+#!/bin/bash
+
 REPOSITORY_NAME=kwangjin-repository
 
 # Repository의 Image 조회
-echo RUN aws ecr list-images
+echo "RUN aws ecr list-images"
 aws ecr list-images --repository-name $REPOSITORY_NAME > list-images.json
 
 # latest Image Id 추출
-echo --- extract Latest Image ID ---
+echo "--- extract Latest Image ID ---"
 LATEST_IMAGE_ID=$(cat list-images.json | jq -c '.imageIds[] | select(.imageTag == "latest")' | jq '.imageDigest')
 
-echo LATEST_IMAGE_ID : $LATEST_IMAGE_ID
+echo "LATEST_IMAGE_ID : ${LATEST_IMAGE_ID}"
 
 # wait for scan result
-echo --- RUN aws ecr wait ---
+echo "--- RUN aws ecr wait ---"
 
 aws ecr wait image-scan-complete --repository-name $REPOSITORY_NAME --image-id imageDigest=$LATEST_IMAGE_ID
 
-echo --- COMPLETE aws ecr wait ---
+echo "--- COMPLETE aws ecr wait ---"
 
 # get scan result
-echo --- RUN aws ecr describe-image-scan-findings ---
+echo "--- RUN aws ecr describe-image-scan-findings ---"
 
 aws ecr describe-image-scan-findings --repository-name $REPOSITORY_NAME --image-id imageDigest=$LATEST_IMAGE_ID > describe-image.json
 
 
 # find Severity Counts
-echo --- find SeverityCounts ---
+echo "--- find SeverityCounts ---"
 
 SEVERITY_COUNTS_OBJECT=$(cat describe-image.json | jq '.imageScanFindings.findingSeverityCounts')
-echo $SEVERITY_COUNTS_OBJECT
-MEDIUM_VALUE=$(echo $SEVERITY_COUNTS_OBJECT | jq '.MEDIUM // 0')
-HIGH_VALUE=$(echo $SEVERITY_COUNTS_OBJECT | jq '.HIGH // 0')
-CRITICAL_VALUE=$(echo $SEVERITY_COUNTS_OBJECT | jq '.CRITICAL // 0')
+echo "${SEVERITY_COUNTS_OBJECT}"
+MEDIUM_VALUE=$(echo "${SEVERITY_COUNTS_OBJECT}" | jq '.MEDIUM // 0')
+HIGH_VALUE=$(echo "${SEVERITY_COUNTS_OBJECT}}" | jq '.HIGH // 0')
+CRITICAL_VALUE=$(echo "${SEVERITY_COUNTS_OBJECT}" | jq '.CRITICAL // 0')
 
-echo MEDIUM_VALUE : $MEDIUM_VALUE
-echo HIGH_VALUE : $HIGH_VALUE
-echo CRITICAL_VALUE : $CRITICAL_VALUE
+echo "MEDIUM_VALUE : ${MEDIUM_VALUE}"
+echo "HIGH_VALUE : ${HIGH_VALUE}"
+echo "CRITICAL_VALUE : ${CRITICAL_VALUE}"
 
 # result Severity
-echo --- result Severity ---
+echo "--- result Severity ---"
 
 SEVERITY_COUNTS=$(($MEDIUM_VALUE + $HIGH_VALUE + $CRITICAL_VALUE))
 
-echo SEVERITY_COUNT : $SEVERITY_COUNTS
+echo "SEVERITY_COUNT : ${SEVERITY_COUNTS}"
 
 if [ $SEVERITY_COUNTS -eq 0 ]
 then
